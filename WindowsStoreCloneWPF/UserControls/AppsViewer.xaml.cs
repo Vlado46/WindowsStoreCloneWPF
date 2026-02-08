3e9cdccec15a1606e1b660dcb5b9a9b30004f53d
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2026-02-08 21:10:28 +0100

    Add project files.

---
+++ WindowsStoreCloneWPF/UserControls/AppsViewer.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsStoreCloneWPF.UserControls
+{
+    public partial class AppsViewer : UserControl
+    {
+        List<AnApp> PresentedApps;
+        public AppsViewer()
+        {
+            InitializeComponent();
+            PresentedApps = new List<AnApp>();
+            AppsList.ItemsSource = PresentedApps;
+            for (int i = 0; i < 9; i++)
+            {
+                AnApp curr = new AnApp();
+                PresentedApps.Add(curr);
+            }
+        }
+
+        private void ScrollLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            int widthOfOneApp = (int)PresentedApps.First().ActualWidth 
+                + 2 * (int)PresentedApps.First().Margin.Left;
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset - 1 * widthOfOneApp);
+        }
+
+        private void ScrollRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            int widthOfOneApp = (int)PresentedApps.First().ActualWidth
+                + 2 * (int)PresentedApps.First().Margin.Left;
+            AppsScrollView.ScrollToHorizontalOffset(AppsScrollView.HorizontalOffset + 1 * widthOfOneApp);
+        }
+    }
+}
